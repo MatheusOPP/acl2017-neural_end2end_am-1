19dd2524c5f0efe838528251622dd972a8c1063c
Matheus de Oliveira <matheusolipaula@gmail.com>
2024-03-11 00:44:16 +0100

    Add arguments and refactor data loading and tokenization

--- progs/BERT/train_and_eval.py
+++ progs/BERT/train_and_eval.py
@@ -7,7 +7,20 @@ import pandas as pd
 from tqdm import tqdm
 import numpy as np
 from sklearn.model_selection import train_test_split
+import argparse
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Argument Parser for Training and Testing')
+
+    parser.add_argument('--train_file', type=str, required=True, help='Path to the training file')
+    parser.add_argument('--test_file', type=str, required=True, help='Path to the testing file')
+    parser.add_argument('--n_epochs', type=int, default=10, help='Number of epochs for training (default: 10)')
+    parser.add_argument('--batch_size', type=int, default=16, help='Batch size for training (default: 16)')
+    parser.add_argument('--target', type=str, choices=['component', 'relation', 'all'], default='all',
+                        help="Target to train for, can be 'component', 'relation', or 'all' (default: all)")
+    parser.add_argument('--output_file', type=str, help='Path to the output file')
+
+    return parser.parse_args()
 
 def accuracy_score(y_true, y_pred):
     total_samples = sum(len(sublist) - np.count_nonzero(sublist == -100) for sublist in y_true)
@@ -47,36 +60,57 @@ def f1_score(y_true, y_pred):
     
     return f1
 
-def read_dataset(file_path):
+def read_dataset(file_path, target='component'):
     with open(file_path, 'r') as file:
-        data = file.read().strip().split('\n\n')  # Split by empty lines to get paragraphs
-        paragraphs = []
-        paragraphs_labels = []
-        for paragraph in data:
-            sentences = paragraph.strip().split('\n')  # Split by lines to get sentences
+        splits = file.read().strip().split('\n\n')  # Split by empty lines to get paragraphs
+        data = []
+        tags = []
+        for split in splits:
+            rows = split.strip().split('\n')  # Split by lines to get sentences
             tokens = []
             labels = []
-            for sentence in sentences:
-                parts = sentence.split('\t')
+            for row in rows:
+                parts = row.split('\t')
                 tokens.append(parts[1])  # Add token
-                labels.append(parts[-1].split(":")[0])
-            paragraphs.append(tokens)  # Concatenate tokens into a single string for each paragraph
-            paragraphs_labels.append(labels)
-        return paragraphs, paragraphs_labels
+                if target == 'component':
+                    labels.append(parts[-1].split(":")[0])
+                elif target == 'relation':
+                    labels.append(':'.join(parts[-1].split(":")[-2:]) if "MajorClaim" not in parts[-1] else 'O')
+                elif target == 'all':
+                    labels.append(parts[-1])
+            data.append(tokens)  # Concatenate tokens into a single string for each paragraph
+            tags.append(labels)
+        return data, tags    
     
-def encode_tags(tags, encodings):
-    labels = [[tag2id[tag] for tag in doc] for doc in tags]
-    encoded_labels = []
-    for doc_labels, doc_offset in zip(labels, encodings.offset_mapping):
-        # create an empty array of -100
-        doc_enc_labels = np.ones(len(doc_offset),dtype=int) * -100
-        arr_offset = np.array(doc_offset)
 
-        # set labels whose first offset position is 0 and the second is not 0
-        doc_enc_labels[(arr_offset[:,0] == 0) & (arr_offset[:,1] != 0)] = doc_labels
-        encoded_labels.append(doc_enc_labels.tolist())
 
-    return encoded_labels
+def tokenize_and_align_labels(tags, tokenized_inputs, label_all_tokens=True):
+    tags_ids = [[tag2id[tag] for tag in doc] for doc in tags]
+
+    labels = []
+    for i, label in enumerate(tags_ids):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+            # ignored in the loss function.
+            if word_idx is None:
+                label_ids.append(-100)
+            # We set the label for the first token of each word.
+            elif word_idx != previous_word_idx:
+                label_ids.append(label[word_idx])
+            # For the other tokens in a word, we set the label to either the current label or -100, depending on
+            # the label_all_tokens flag.
+            else:
+                label_ids.append(label[word_idx] if label_all_tokens else -100)
+            previous_word_idx = word_idx
+
+        labels.append(label_ids)
+        
+    return labels
+
+
 
 class PersuasiveEssaysDataset(torch.utils.data.Dataset):
     def __init__(self, encodings, labels):
@@ -91,32 +125,37 @@ class PersuasiveEssaysDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+args = parse_arguments()
 
 model_name = 'distilbert-base-uncased'
 tokenizer = DistilBertTokenizerFast.from_pretrained(model_name)
 
 # Example usage:
-train_file_path = 'data/conll/Paragraph_Level/train.dat' 
-test_file_path = 'data/conll/Paragraph_Level/test.dat' 
+train_file_path = args.train_file
+test_file_path = args.test_file 
 
-X_train, y_train = read_dataset(train_file_path)
-X_test, y_test = read_dataset(test_file_path)
+target = args.target
 
-max_seq_length = max(len(tokens) for tokens in X_train+X_test)+11
+X_train, y_train = read_dataset(train_file_path, target=target)
+X_test, y_test = read_dataset(test_file_path, target=target)
 
-unique_tags = set(tag for doc in y_train for tag in doc)
+max_seq_length = min(max(len(tokens) for tokens in X_train+X_test), 512)
+
+unique_tags = set(tag for doc in y_train+y_test for tag in doc)
 tag2id = {tag: id for id, tag in enumerate(unique_tags)}
 id2tag = {id: tag for tag, id in tag2id.items()}
 
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
 
-train_encodings = tokenizer.batch_encode_plus(X_train, is_split_into_words=True, add_special_tokens=True, return_offsets_mapping=True, max_length=max_seq_length, padding='max_length')        # Tokenize and pad labels"
-val_encodings = tokenizer.batch_encode_plus(X_val, is_split_into_words=True, add_special_tokens=True, return_offsets_mapping=True, max_length=max_seq_length, padding='max_length')
-test_encodings = tokenizer.batch_encode_plus(X_test, is_split_into_words=True, add_special_tokens=True, return_offsets_mapping=True, max_length=max_seq_length, padding='max_length')
+train_encodings = tokenizer.batch_encode_plus(X_train, is_split_into_words=True, add_special_tokens=True, truncation=True, return_offsets_mapping=True, max_length=max_seq_length, padding='max_length')        # Tokenize and pad labels"
+val_encodings = tokenizer.batch_encode_plus(X_val, is_split_into_words=True, add_special_tokens=True, truncation=True, return_offsets_mapping=True, max_length=max_seq_length, padding='max_length')
+test_encodings = tokenizer.batch_encode_plus(X_test, is_split_into_words=True, add_special_tokens=True, truncation=True, return_offsets_mapping=True, max_length=max_seq_length, padding='max_length')
 
-train_labels = encode_tags(y_train, train_encodings)
-val_labels = encode_tags(y_val, val_encodings)
-test_labels = encode_tags(y_test, test_encodings)
+train_labels = tokenize_and_align_labels(y_train, train_encodings)
+val_labels = tokenize_and_align_labels(y_val, val_encodings)
+test_labels = tokenize_and_align_labels(y_test, test_encodings)
+
+print(train_labels)
 
 train_encodings.pop("offset_mapping") # we don't want to pass this to the model
 val_encodings.pop("offset_mapping")
@@ -137,9 +176,9 @@ model.to(device)
 
 optim = torch.optim.AdamW(model.parameters(), lr=5e-5)
 
-epochs = 100
+epochs = args.n_epochs
 
-for epoch in tqdm(range(5)):
+for epoch in tqdm(range(epochs)):
     model.train()
     
     train_predictions = []
@@ -190,4 +229,4 @@ for epoch in tqdm(range(5)):
     
     print(f"Epoch {epoch+1}, Training Loss:' {loss:.4f}, Training Accuracy: {train_accuracy:.4f}, Validation Loss: {avg_val_loss:.4f}, Validation Accuracy: {val_accuracy:.4f}, Val F1-Score: {val_f1:.4f}")
     
-    model.save_pretrained('models/distilbert/seqtag_pl_cp')
+    model.save_pretrained(args.output_file)
